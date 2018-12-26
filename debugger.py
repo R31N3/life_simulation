fc19b5b82f1c9f743880d11b0a4ffafa1b4b6d8b
ShagonRU <goshan.chamor@yandex.ru>
2018-12-26 21:46:04 +0300

    *push

--- debugger.py
+++ debugger.py
@@ -71,7 +71,7 @@ def main():
     print("DE: Введите ID пользователя")
     id = input()
     stResponce = DeResponse()
-    database = init_database(host='localhost', user='shagonru', password='13082000',
+    database = init_database(host='localhost', user='postgres3', password='1488',
                              dbname='programmer_simulator')
     responce, userStorage = handle_dialog(DeRequest(True, id), stResponce, {}, database)
     printResponce(responce)
